8e0f296d624fcc5e0741dc144af9c213662d925e
Bruno Benicio - Escritório <carecsnay1@gmail.com>
2024-12-19 15:08:33 -0300

    Chore: Select elements with JS.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("Salve salve");
+const dqs = (element) => document.querySelector(element);
+
+const formElement = dqs(".form-container");
+const userNameInput = dqs("#user-name");
+const userEmailInput = dqs("#user-email");
+const userPasswordInput = dqs("#user-password");
+const userCheckPasswordInput = dqs("#user-check-password");
+const buttonElement = dqs("button");
